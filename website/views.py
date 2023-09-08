a4d96caf59f43bc922ff7779c5ab9ccffb2229c0
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-09-08 11:55:00 +0400

    Adding base.html temple

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # creating a blueprint for the website
 
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home_page():
-    return "<h1>Test<h1>"
+    return render_template("home_page.html")
